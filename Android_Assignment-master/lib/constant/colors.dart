d9754bb15e27f9b5339e42c0afc406fccdfe164f
samyog27 <samyog.shah21@gmail.com>
2022-09-12 21:35:26 +0545

    Blood Donation

---
+++ Android_Assignment-master/lib/constant/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static const backgroundColor = Color.fromARGB(255, 241, 235, 235);
+  static const redColor = Color.fromARGB(255, 36, 83, 170);
+}
